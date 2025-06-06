3ef3177359a3bd4b2aba8fa9cacce0bcb30167a3
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-06-06 04:01:30 +0200

    ⠀⠀⠀⠀⠀

--- Runtime/UIToolkitExtensions.cs
+++ Runtime/UIToolkitExtensions.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -6,74 +5,41 @@ namespace UnityEssentials
 {
     public static class UIToolkitExtensions
     {
-        public static VisualElement SetSize(this VisualElement element, Vector2 size) =>
-            SetSize(element, size.x, size.y);
+        // StyleSheet
+        public static void AddStyleSheet(this UIDocument document, StyleSheet styleSheet) => document.rootVisualElement.styleSheets.Add(styleSheet);
 
-        public static VisualElement SetSize(this VisualElement element, float widthPercent, float heightPercent)
+        // Color
+        public static void SetColor(this VisualElement element, Color color) => element.style.color = color;
+        public static Color GetColor(this VisualElement element) => element.resolvedStyle.color;
+        public static void SetBackgroundColor(this VisualElement element, Color color) => element.style.backgroundColor = color;
+        public static Color GetBackgroundColor(this VisualElement element) => element.resolvedStyle.backgroundColor;
+
+        // Size
+        public static void SetSize(this VisualElement element, Vector2 size) => SetSize(element, size.x, size.y);
+
+        public static void SetSize(this VisualElement element, float widthPercent, float heightPercent)
         {
             SetWidth(element, widthPercent);
             SetHeight(element, heightPercent);
-
-            return element;
         }
 
-        public static VisualElement SetSize(this VisualElement element, int width, int height)
+        public static void SetSize(this VisualElement element, int width, int height)
         {
             SetWidth(element, width);
             SetHeight(element, height);
-
-            return element;
         }
 
-        public static VisualElement SetWidth(this VisualElement element, float widthPercent)
-        {
-            element.style.width = Length.Percent(widthPercent);
+        public static void SetWidth(this VisualElement element, float widthPercent) => element.style.width = Length.Percent(widthPercent);
+        public static void SetWidth(this VisualElement element, int width) => element.style.width = width;
+        public static void SetHeight(this VisualElement element, float heightPercent) => element.style.height = Length.Percent(heightPercent);
+        public static void SetHeight(this VisualElement element, int height) => element.style.height = height;
 
-            return element;
-        }
+        // Image
+        public static void SetImage(this VisualElement element, Texture2D texture) => element.style.backgroundImage = new StyleBackground(texture);
+        public static void SetImage(this VisualElement element, RenderTexture renderTexture) => element.style.backgroundImage = new StyleBackground(Background.FromRenderTexture(renderTexture));
 
-        public static VisualElement SetWidth(this VisualElement element, int width)
-        {
-            element.style.width = width;
-
-            return element;
-        }
-
-        public static VisualElement SetHeight(this VisualElement element, float heightPercent)
-        {
-            element.style.height = Length.Percent(heightPercent);
-
-            return element;
-        }
-
-        public static VisualElement SetHeight(this VisualElement element, int height)
-        {
-            element.style.height = height;
-
-            return element;
-        }
-
-        public static VisualElement SetBackgroundColor(this VisualElement element, Color color)
-        {
-            element.style.backgroundColor = color;
-
-            return element;
-        }
-
-        public static VisualElement SetImage(this VisualElement element, Texture2D texture)
-        {
-            element.style.backgroundImage = new StyleBackground(texture);
-
-            return element;
-        }
-
-        public static VisualElement SetImage(this VisualElement element, RenderTexture renderTexture)
-        {
-            element.style.backgroundImage = new StyleBackground(Background.FromRenderTexture(renderTexture));
-            return element;
-        }
-
-        public static void AddStyleSheet(this UIDocument document, StyleSheet styleSheet) =>
-            document.rootVisualElement.styleSheets.Add(styleSheet);
+        // Display
+        public static void SetDisplayEnabled(this VisualElement element, bool enabled) => element.style.display = enabled ? DisplayStyle.Flex : DisplayStyle.None;
+        public static bool GetDisplayEnabled(this VisualElement element) => element.resolvedStyle.display != DisplayStyle.None;
     }
 }
