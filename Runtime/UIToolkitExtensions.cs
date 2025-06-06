b158ae289cfeabbdc074e7a151c105b107e92b99
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-06-06 08:06:59 +0200

    ⠀⠀⠀⠀⠀

--- Runtime/UIToolkitExtensions.cs
+++ Runtime/UIToolkitExtensions.cs
@@ -6,7 +6,29 @@ namespace UnityEssentials
     public static class UIToolkitExtensions
     {
         // StyleSheet
-        public static void AddStyleSheet(this UIDocument document, StyleSheet styleSheet) => document.rootVisualElement.styleSheets.Add(styleSheet);
+        public static void AddStyleSheet(this UIDocument document, StyleSheet styleSheet)
+        {
+            if (styleSheet != null && !document.rootVisualElement.styleSheets.Contains(styleSheet))
+                document.rootVisualElement.styleSheets.Add(styleSheet);
+        }
+
+        // Size
+        public static void SetSize(this VisualElement element, Vector2 size) => SetSize(element, size.x, size.y);
+        public static void SetSize(this VisualElement element, float widthPercent, float heightPercent)
+        {
+            SetWidth(element, widthPercent);
+            SetHeight(element, heightPercent);
+        }
+        public static void SetSize(this VisualElement element, int width, int height)
+        {
+            SetWidth(element, width);
+            SetHeight(element, height);
+        }
+        public static void SetWidth(this VisualElement element, float widthPercent) => element.style.width = Length.Percent(widthPercent);
+        public static void SetWidth(this VisualElement element, int width) => element.style.width = width;
+        public static void SetHeight(this VisualElement element, float heightPercent) => element.style.height = Length.Percent(heightPercent);
+        public static void SetHeight(this VisualElement element, int height) => element.style.height = height;
+
 
         // Color
         public static void SetColor(this VisualElement element, Color color) => element.style.color = color;
@@ -14,29 +36,17 @@ namespace UnityEssentials
         public static void SetBackgroundColor(this VisualElement element, Color color) => element.style.backgroundColor = color;
         public static Color GetBackgroundColor(this VisualElement element) => element.resolvedStyle.backgroundColor;
 
-        // Size
-        public static void SetSize(this VisualElement element, Vector2 size) => SetSize(element, size.x, size.y);
-
-        public static void SetSize(this VisualElement element, float widthPercent, float heightPercent)
-        {
-            SetWidth(element, widthPercent);
-            SetHeight(element, heightPercent);
-        }
-
-        public static void SetSize(this VisualElement element, int width, int height)
-        {
-            SetWidth(element, width);
-            SetHeight(element, height);
-        }
-
-        public static void SetWidth(this VisualElement element, float widthPercent) => element.style.width = Length.Percent(widthPercent);
-        public static void SetWidth(this VisualElement element, int width) => element.style.width = width;
-        public static void SetHeight(this VisualElement element, float heightPercent) => element.style.height = Length.Percent(heightPercent);
-        public static void SetHeight(this VisualElement element, int height) => element.style.height = height;
-
         // Image
-        public static void SetImage(this VisualElement element, Texture2D texture) => element.style.backgroundImage = new StyleBackground(texture);
-        public static void SetImage(this VisualElement element, RenderTexture renderTexture) => element.style.backgroundImage = new StyleBackground(Background.FromRenderTexture(renderTexture));
+        public static void SetBackgroundImage(this VisualElement element, Texture2D texture)
+        {
+            if (texture != null)
+                element.style.backgroundImage = new StyleBackground(texture);
+        }
+        public static void SetBackgroundImage(this VisualElement element, RenderTexture renderTexture)
+        {
+            if (renderTexture != null)
+                element.style.backgroundImage = new StyleBackground(Background.FromRenderTexture(renderTexture));
+        }
 
         // Display
         public static void SetDisplayEnabled(this VisualElement element, bool enabled) => element.style.display = enabled ? DisplayStyle.Flex : DisplayStyle.None;
